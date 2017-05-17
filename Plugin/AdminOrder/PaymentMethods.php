e711e8fc99814b4ff46e2f544c5417f6c347f394
John Sparwasser <johnspar1@gmail.com>
2017-05-16 20:41:15 -0400

    PHPDocs and cleanup

--- Plugin/AdminOrder/PaymentMethods.php
+++ Plugin/AdminOrder/PaymentMethods.php
@@ -5,12 +5,12 @@ namespace Swarming\SubscribePro\Plugin\AdminOrder;
 class PaymentMethods
 {
     /**
-     * @var Swarming\SubscribePro\Helper\QuoteItem
+     * @var \Swarming\SubscribePro\Helper\QuoteItem
      */
     protected $quoteItemHelper;
 
     /**
-     * @param \Swarming\SubscribePro\Helper\OrderItem $orderItemHelper
+     * @param \Swarming\SubscribePro\Helper\QuoteItem $quoteItemHelper
      */
     public function __construct(
         \Swarming\SubscribePro\Helper\QuoteItem $quoteItemHelper
@@ -19,8 +19,11 @@ class PaymentMethods
     }
 
     /**
+     * Plugin for the getMethods() function
+     *
      * @param \Magento\Sales\Block\Adminhtml\Order\Create\Billing\Method\Form $subject
      * @param \Closure $proceed
+     * @return array
      */
     public function aroundGetMethods(
         \Magento\Sales\Block\Adminhtml\Order\Create\Billing\Method\Form $subject,
@@ -34,6 +37,12 @@ class PaymentMethods
         return $methods;
     }
 
+    /**
+     * Checks to see if a subscription is present in the quote items
+     *
+     * @param \Magento\Eav\Model\Entity\Collection\AbstractCollection|null $items
+     * @return bool
+     */
     protected function subscriptionIsPresent($items)
     {
         if ($items == null) {
@@ -47,6 +56,12 @@ class PaymentMethods
         return false;
     }
 
+    /**
+     * Filters out the payment methods other than the SubscribePro ones
+     *
+     * @param array $methods
+     * @return array
+     */
     protected function filterOutMethods(array $methods)
     {
         $subscribeProMethods = [];
