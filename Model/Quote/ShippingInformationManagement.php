3ee21a0d44f83016efcefb25ce0fcfd81484d64a
David King <imkingdavid@gmail.com>
2018-04-03 20:07:39 +0000

    [#156482670] Ensure the region ID is set in Magento on the shipping address for recurring orders

---
+++ Model/Quote/ShippingInformationManagement.php
@@ -0,0 +1,110 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Swarming\SubscribePro\Model\Quote;
+
+use Magento\Framework\Exception\InputException;
+use Magento\Framework\Exception\StateException;
+use Magento\Framework\Exception\NoSuchEntityException;
+use Magento\Quote\Api\Data\AddressInterface;
+use Magento\Quote\Api\Data\CartInterface;
+use Psr\Log\LoggerInterface as Logger;
+use Magento\Quote\Model\QuoteAddressValidator;
+use Magento\Quote\Api\Data\CartExtensionFactory;
+use Magento\Quote\Model\ShippingAssignmentFactory;
+use Magento\Quote\Model\ShippingFactory;
+use Magento\Framework\App\ObjectManager;
+use Magento\Directory\Model\RegionFactory;
+
+/**
+ * @SuppressWarnings(PHPMD.CouplingBetweenObjects)
+ */
+class ShippingInformationManagement extends \Magento\Checkout\Model\ShippingInformationManagement
+{
+    /**
+     * @var \Magento\Directory\Model\RegionFactory
+     */
+    protected $regionFactory;
+
+    /**
+     * Constructor
+     *
+     * @param \Magento\Quote\Api\PaymentMethodManagementInterface $paymentMethodManagement
+     * @param \Magento\Checkout\Model\PaymentDetailsFactory $paymentDetailsFactory
+     * @param \Magento\Quote\Api\CartTotalRepositoryInterface $cartTotalsRepository
+     * @param \Magento\Quote\Api\CartRepositoryInterface $quoteRepository
+     * @param \Magento\Quote\Model\QuoteAddressValidator $addressValidator
+     * @param Logger $logger
+     * @param \Magento\Customer\Api\AddressRepositoryInterface $addressRepository
+     * @param \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig
+     * @param \Magento\Quote\Model\Quote\TotalsCollector $totalsCollector
+     * @param CartExtensionFactory|null $cartExtensionFactory,
+     * @param ShippingAssignmentFactory|null $shippingAssignmentFactory,
+     * @param ShippingFactory|null $shippingFactory,
+     * @param RegionFactory|null $regionFactory
+     * @SuppressWarnings(PHPMD.ExcessiveParameterList)
+     */
+    public function __construct(
+        \Magento\Quote\Api\PaymentMethodManagementInterface $paymentMethodManagement,
+        \Magento\Checkout\Model\PaymentDetailsFactory $paymentDetailsFactory,
+        \Magento\Quote\Api\CartTotalRepositoryInterface $cartTotalsRepository,
+        \Magento\Quote\Api\CartRepositoryInterface $quoteRepository,
+        QuoteAddressValidator $addressValidator,
+        Logger $logger,
+        \Magento\Customer\Api\AddressRepositoryInterface $addressRepository,
+        \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
+        \Magento\Quote\Model\Quote\TotalsCollector $totalsCollector,
+        CartExtensionFactory $cartExtensionFactory = null,
+        ShippingAssignmentFactory $shippingAssignmentFactory = null,
+        ShippingFactory $shippingFactory = null,
+        RegionFactory $regionFactory = null
+    ) {
+        $this->paymentMethodManagement = $paymentMethodManagement;
+        $this->paymentDetailsFactory = $paymentDetailsFactory;
+        $this->cartTotalsRepository = $cartTotalsRepository;
+        $this->quoteRepository = $quoteRepository;
+        $this->addressValidator = $addressValidator;
+        $this->logger = $logger;
+        $this->addressRepository = $addressRepository;
+        $this->scopeConfig = $scopeConfig;
+        $this->totalsCollector = $totalsCollector;
+        $this->cartExtensionFactory = $cartExtensionFactory ?: ObjectManager::getInstance()
+            ->get(CartExtensionFactory::class);
+        $this->shippingAssignmentFactory = $shippingAssignmentFactory ?: ObjectManager::getInstance()
+            ->get(ShippingAssignmentFactory::class);
+        $this->shippingFactory = $shippingFactory ?: ObjectManager::getInstance()
+            ->get(ShippingFactory::class);
+        $this->regionFactory = $regionFactory ?: ObjectManager::getInstance()
+            ->get(RegionFactory::class);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function saveAddressInformation(
+        $cartId,
+        \Magento\Checkout\Api\Data\ShippingInformationInterface $addressInformation
+    ) {
+        $shippingAddress = $addressInformation->getShippingAddress();
+        $this->logger->info('SUBSCRIBEPRO Region ID Patch');
+        $this->logger->info('Checking Current Region ID... ' . $shippingAddress->getRegionId());
+
+        $regionId = $shippingAddress->getRegionId();
+        if (!$regionId) {
+            $this->logger->info('No region ID set, loading from database...');
+            // Get Region ID from Database if not present
+            $region = $this->regionFactory->create();
+            $regionId = $region->loadByCode($shippingAddress->getRegionCode(), $shippingAddress->getCountryId())->getId();
+
+            $this->logger->info('Loaded region with ID ' . $regionId . ' using region code: ' . $shippingAddress->getRegionCode());
+            $this->logger->info('Setting region ID on shipping address');
+            $shippingAddress->setRegionId($regionId);
+            $addressInformation->setShippingAddress($shippingAddress);
+        }
+
+        return parent::saveAddressInformation($cartId, $addressInformation);
+    }
+
+}
