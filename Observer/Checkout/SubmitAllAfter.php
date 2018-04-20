5e9f4bb70d6a247d36c8ce6ac2bf5efe2ff2e456
David King <imkingdavid@gmail.com>
2018-04-20 17:21:55 +0000

    [#156772540] Include configurable product options on subscription created by admin order

--- Observer/Checkout/SubmitAllAfter.php
+++ Observer/Checkout/SubmitAllAfter.php
@@ -25,6 +25,11 @@ class SubmitAllAfter implements ObserverInterface
     protected $subscriptionCreator;
 
     /**
+     * @var \Magento\Quote\Model\Quote\Item\CartItemOptionsProcessor
+     */
+    protected $cartItemOptionProcessor;
+
+    /**
      * @var \Psr\Log\LoggerInterface
      */
     protected $logger;
@@ -33,17 +38,20 @@ class SubmitAllAfter implements ObserverInterface
      * @param \Swarming\SubscribePro\Model\Config\General $generalConfig
      * @param \Magento\Checkout\Model\Session $checkoutSession
      * @param \Swarming\SubscribePro\Model\Quote\SubscriptionCreator $subscriptionCreator
+     * @param \Magento\Quote\Model\Quote\Item\CartItemOptionsProcessor $cartItemOptionProcessor
      * @param \Psr\Log\LoggerInterface $logger
      */
     public function __construct(
         \Swarming\SubscribePro\Model\Config\General $generalConfig,
         \Magento\Checkout\Model\Session $checkoutSession,
         \Swarming\SubscribePro\Model\Quote\SubscriptionCreator $subscriptionCreator,
+        \Magento\Quote\Model\Quote\Item\CartItemOptionsProcessor $cartItemOptionProcessor,
         \Psr\Log\LoggerInterface $logger
     ) {
         $this->generalConfig = $generalConfig;
         $this->checkoutSession = $checkoutSession;
         $this->subscriptionCreator = $subscriptionCreator;
+        $this->cartItemOptionProcessor = $cartItemOptionProcessor;
         $this->logger = $logger;
     }
 
@@ -55,6 +63,7 @@ class SubmitAllAfter implements ObserverInterface
     {
         /** @var \Magento\Quote\Model\Quote $quote */
         $quote = $observer->getData('quote');
+        $this->addProductOptionsToQuoteItems($quote);
 
         /** @var \Magento\Sales\Api\Data\OrderInterface $order */
         $order = $observer->getData('order');
@@ -73,6 +82,22 @@ class SubmitAllAfter implements ObserverInterface
             $this->checkoutSession->setData(SubscriptionCreator::FAILED_SUBSCRIPTION_COUNT, $result[SubscriptionCreator::FAILED_SUBSCRIPTION_COUNT]);
         } catch (\Exception $e) {
             $this->logger->critical($e);
+        }
+    }
+
+    /**
+     * @param \Magento\Quote\Model\Quote $quote
+     * @return void
+     */
+    private function addProductOptionsToQuoteItems($quote)
+    {
+        foreach ($quote->getAllItems() as $quoteItem) {
+            if ($quoteItem->getProductOption()) { // Skip if options are already added
+                continue;
+            }
+            /** @var \Magento\Quote\Model\Quote\Item $quoteItem */
+            $item = $this->cartItemOptionProcessor->addProductOptions($quoteItem->getProductType(), $quoteItem);
+            $this->cartItemOptionProcessor->applyCustomOptions($item);
         }
     }
 }
