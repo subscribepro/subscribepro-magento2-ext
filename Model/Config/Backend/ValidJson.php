657ca296442faa4c6e483854ef10cbd15552f343
David King <imkingdavid@gmail.com>
2020-05-06 12:09:04 -0400

    [#172539318] Improved 'invalid JSON' error message, added 'use' statements

--- Model/Config/Backend/ValidJson.php
+++ Model/Config/Backend/ValidJson.php
@@ -2,25 +2,30 @@
 
 namespace Swarming\SubscribePro\Model\Config\Backend;
 
+use Magento\Framework\App\Config\Value;
+use Magento\Framework\Exception\ValidatorException;
+
 /**
  * Encrypted config field backend model.
  *
  * @api
  * @since 100.0.2
  */
-class ValidJson extends \Magento\Framework\App\Config\Value
+class ValidJson extends Value
 {
     /**
      * Encrypt value before saving
      *
      * @return void
-     * @throws \Magento\Framework\Exception\ValidatorException
+     * @throws ValidatorException
      */
     public function beforeSave()
     {
         $jsonDecodedValue = json_decode($this->getValue(), true);
         if (json_last_error() !== JSON_ERROR_NONE) {
-            throw new \Magento\Framework\Exception\ValidatorException(__('Invalid JSON value. ' . $this->getData('label')));
+            $field = $this->getFieldConfig();
+            $label = $field && is_array($field) ? ' in field "' . $field['label'] . '"' : 'value';
+            throw new ValidatorException(__('Invalid JSON ' . $label));
         }
 
         $this->setValue(json_encode($jsonDecodedValue, JSON_PRETTY_PRINT));
