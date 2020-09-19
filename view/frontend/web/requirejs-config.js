2f3367b4e47062cfc6a7fc815c965f32621aa444
David King <imkingdavid@gmail.com>
2020-09-19 00:38:56 -0400

    Revert "[#174888328] Fix shipping method code set in checkout on Magento 2.3.5+"
    Determined it wasn't working as intended and couldn't figure out the fix right away, need to revisit.
    
    This reverts commit 9adcdb9836924df5725f9f05c28f277b5a760f5e.

--- view/frontend/web/requirejs-config.js
+++
@@ -1,9 +0,0 @@
-var config = {
-    config: {
-        mixins: {
-            'Magento_Checkout/js/action/set-payment-information': {
-                'Swarming_SubscribePro/js/action/checkout/set-payment-information-mixin': true
-            }
-        }
-    }
-};
