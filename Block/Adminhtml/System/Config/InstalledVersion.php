89eb16e0f520ceb6e466da9f22fb18ae2a887a65
David King <imkingdavid@gmail.com>
2020-09-21 12:05:08 -0400

    [#174908047] Show installed extension version in Magento 2 extension config

---
+++ Block/Adminhtml/System/Config/InstalledVersion.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Swarming\SubscribePro\Block\Adminhtml\System\Config;
+
+class InstalledVersion extends \Magento\Config\Block\System\Config\Form\Field
+{
+    const INSTALLED_VERSION = '1.3.4';
+
+    protected function _getElementHtml(\Magento\Framework\Data\Form\Element\AbstractElement $element)
+    {
+        $element->setValue(self::INSTALLED_VERSION);
+
+        return '<strong>' . $element->getEscapedValue() . '</strong> - [<a href="https://github.com/subscribepro/subscribepro-magento2-ext/releases">' . 'View Releases' . '</a>]';
+    }
+}
