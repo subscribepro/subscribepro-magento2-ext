3441e2f29f60b226961d237b3deca5fa10c4af2a
EugeneShab <dev.eugene.shab@gmail.com>
2021-02-15 20:55:35 +0200

    Added missed condition.

--- Observer/Checkout/SubmitAllAfter.php
+++ Observer/Checkout/SubmitAllAfter.php
@@ -4,6 +4,7 @@ namespace Swarming\SubscribePro\Observer\Checkout;
 
 use Magento\Framework\Event\Observer;
 use Magento\Framework\Event\ObserverInterface;
+use Swarming\SubscribePro\Gateway\Config\ApplePayConfigProvider;
 use Swarming\SubscribePro\Model\Quote\SubscriptionCreator;
 use Swarming\SubscribePro\Gateway\Config\ConfigProvider as GatewayConfigProvider;
 
@@ -71,6 +72,7 @@ class SubmitAllAfter implements ObserverInterface
         $websiteCode = $quote->getStore()->getWebsite()->getCode();
         if (!$this->generalConfig->isEnabled($websiteCode)
             || $order->getPayment()->getMethod() != GatewayConfigProvider::CODE
+            || $order->getPayment()->getMethod() != ApplePayConfigProvider::CODE
             || !$quote->getCustomerId()
         ) {
             return;
