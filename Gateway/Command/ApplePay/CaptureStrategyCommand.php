41e31cedd79ba8c6fd19052fa36ac2740000bc71
EugeneShab <dev.eugene.shab@gmail.com>
2021-02-23 20:15:50 +0200

    [#176922088] Purchase Order

---
+++ Gateway/Command/ApplePay/CaptureStrategyCommand.php
@@ -0,0 +1,61 @@
+<?php
+declare(strict_types=1);
+
+namespace Swarming\SubscribePro\Gateway\Command\ApplePay;
+
+use Magento\Payment\Gateway\CommandInterface;
+use Magento\Payment\Gateway\Helper\ContextHelper;
+use Magento\Sales\Model\Order\Payment;
+
+class CaptureStrategyCommand implements CommandInterface
+{
+    const PURCHASE = 'purchase';
+    const SETTLEMENT = 'settlement';
+
+    /**
+     * @var \Magento\Payment\Gateway\Command\CommandPoolInterface
+     */
+    protected $commandPool;
+
+    /**
+     * @var \Swarming\SubscribePro\Gateway\Helper\SubjectReader
+     */
+    protected $subjectReader;
+
+    /**
+     * @param \Magento\Payment\Gateway\Command\CommandPoolInterface $commandPool
+     * @param \Swarming\SubscribePro\Gateway\Helper\SubjectReader $subjectReader
+     */
+    public function __construct(
+        \Magento\Payment\Gateway\Command\CommandPoolInterface $commandPool,
+        \Swarming\SubscribePro\Gateway\Helper\SubjectReader $subjectReader
+    ) {
+        $this->commandPool = $commandPool;
+        $this->subjectReader = $subjectReader;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function execute(array $commandSubject)
+    {
+        /** @var \Magento\Payment\Gateway\Data\PaymentDataObjectInterface $paymentDO */
+        $paymentDO = $this->subjectReader->readPayment($commandSubject);
+
+        /** @var \Magento\Sales\Model\Order\Payment $paymentInfo */
+        $paymentInfo = $paymentDO->getPayment();
+        ContextHelper::assertOrderPayment($paymentInfo);
+
+        $command = $this->getCommand($paymentInfo);
+        $this->commandPool->get($command)->execute($commandSubject);
+    }
+
+    /**
+     * @param \Magento\Sales\Model\Order\Payment $payment
+     * @return string
+     */
+    protected function getCommand(Payment $payment)
+    {
+        return $payment->getParentTransactionId() ? self::SETTLEMENT : self::PURCHASE;
+    }
+}
---
+++ Gateway/Command/ApplePay/PurchaseCommand.php
@@ -0,0 +1,40 @@
+<?php
+declare(strict_types=1);
+
+namespace Swarming\SubscribePro\Gateway\Command\ApplePay;
+
+use Exception;
+use Magento\Payment\Gateway\CommandInterface;
+use Magento\Vault\Model\Ui\VaultConfigProvider;
+use Swarming\SubscribePro\Gateway\Command\AbstractProfileCreatorCommand;
+use Swarming\SubscribePro\Gateway\Request\ApplePayPaymentDataBuilder;
+use Swarming\SubscribePro\Gateway\Request\PaymentDataBuilder;
+use Swarming\SubscribePro\Gateway\Request\VaultDataBuilder;
+
+class PurchaseCommand extends AbstractProfileCreatorCommand implements CommandInterface
+{
+    /**
+     * @param array $requestData
+     * @return \SubscribePro\Service\Transaction\TransactionInterface
+     * @throws Exception
+     * @throws \Magento\Framework\Exception\LocalizedException
+     * @throws \SubscribePro\Exception\EntityInvalidDataException
+     * @throws \SubscribePro\Exception\HttpException
+     */
+    protected function processTransaction(array $requestData)
+    {
+        if (empty($requestData[PaymentDataBuilder::PAYMENT_METHOD_TOKEN])) {
+            throw new Exception('Payment token is not passed');
+        }
+        $transaction = $this->platformTransactionService->createTransaction($requestData);
+        if (!empty($requestData[VaultConfigProvider::IS_ACTIVE_CODE]) && $requestData[VaultConfigProvider::IS_ACTIVE_CODE]) {
+            $this->platformTransactionService->purchaseByProfile([
+                VaultDataBuilder::PAYMENT_PROFILE_ID => $requestData[ApplePayPaymentDataBuilder::PLATFORM_PROFILE_ID]
+            ], $transaction);
+        } else {
+            $this->platformTransactionService->purchaseByToken($requestData[PaymentDataBuilder::PAYMENT_METHOD_TOKEN], $transaction);
+        }
+
+        return $transaction;
+    }
+}
