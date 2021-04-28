4dea092c831bf8f78e314239737a50b4997387d4
Yurii Torbyk <yurii@findcanary.com>
2021-04-28 12:37:42 +0300

    SPRO-14: Send platform customer id in _meta.changed_by in API requests

--- Model/Meta/Customer.php
+++ Model/Meta/Customer.php
@@ -12,12 +12,28 @@ class Customer implements \Swarming\SubscribePro\Model\MetaUserInterface
     private $customerSession;
 
     /**
+     * @var \Swarming\SubscribePro\Platform\Manager\Customer
+     */
+    private $platformCustomerManager;
+
+    /**
+     * @var \Psr\Log\LoggerInterface
+     */
+    private $logger;
+
+    /**
      * @param \Magento\Customer\Model\Session $customerSession
+     * @param \Swarming\SubscribePro\Platform\Manager\Customer $platformCustomerManager
+     * @param \Psr\Log\LoggerInterface $logger
      */
     public function __construct(
-        \Magento\Customer\Model\Session $customerSession
+        \Magento\Customer\Model\Session $customerSession,
+        \Swarming\SubscribePro\Platform\Manager\Customer $platformCustomerManager,
+        \Psr\Log\LoggerInterface $logger
     ) {
         $this->customerSession = $customerSession;
+        $this->platformCustomerManager = $platformCustomerManager;
+        $this->logger = $logger;
     }
 
     /**
@@ -38,9 +54,26 @@ class Customer implements \Swarming\SubscribePro\Model\MetaUserInterface
     private function getCustomerMeta(\Magento\Customer\Model\Customer $customer): array
     {
         return [
-            'user_id' => $customer->getId(),
+            'customer_id' => $this->getPlatformCustomerId($customer->getId(), $customer->getWebsiteId()),
             'email' => $customer->getEmail(),
             'full_name' => implode(' ', [$customer->getFirstname(), $customer->getLastname()]),
         ];
+    }
+
+    /**
+     * @param int $customerId
+     * @param int $websiteId
+     * @return int|null
+     */
+    private function getPlatformCustomerId($customerId, $websiteId)
+    {
+        try {
+            $platformCustomer = $this->platformCustomerManager->getCustomerById($customerId, false, $websiteId);
+            $platformCustomerId = $platformCustomer->getId();
+        } catch (\Exception $e) {
+            $this->logger->critical($e);
+            $platformCustomerId = null;
+        }
+        return $platformCustomerId;
     }
 }
