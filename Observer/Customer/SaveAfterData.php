3e1920005220225f36fffd27aa4ed6a3912d8914
Dmitry Pryshchepa <dmitry@findcanary.com>
2021-11-29 17:13:21 +0100

    SPRO-49: Refactor PR #101
    Respond to code review suggestions

--- Observer/Customer/SaveAfterData.php
+++ Observer/Customer/SaveAfterData.php
@@ -5,24 +5,22 @@ namespace Swarming\SubscribePro\Observer\Customer;
 
 use Magento\Framework\Event\Observer;
 use Magento\Framework\Event\ObserverInterface;
-use Swarming\SubscribePro\Model\Config\General as SpGeneralConfig;
-use Swarming\SubscribePro\Platform\Manager\Customer as PlatformManagerCustomer;
-use Swarming\SubscribePro\Platform\Service\Customer as PlatformServiceCustomer;
+use Magento\Customer\Api\Data\CustomerInterface;
 
 class SaveAfterData implements ObserverInterface
 {
     /**
-     * @var SpGeneralConfig
+     * @var \Swarming\SubscribePro\Model\Config\General
      */
     private $generalConfig;
 
     /**
-     * @var PlatformManagerCustomer
+     * @var \Swarming\SubscribePro\Platform\Manager\Customer
      */
     private $platformCustomerManager;
 
     /**
-     * @var PlatformServiceCustomer
+     * @var \Swarming\SubscribePro\Platform\Service\Customer
      */
     private $platformCustomerService;
 
@@ -38,9 +36,9 @@ class SaveAfterData implements ObserverInterface
      * @param \Psr\Log\LoggerInterface $logger
      */
     public function __construct(
-        SpGeneralConfig $generalConfig,
-        PlatformManagerCustomer $platformCustomerManager,
-        PlatformServiceCustomer $platformCustomerService,
+        \Swarming\SubscribePro\Model\Config\General $generalConfig,
+        \Swarming\SubscribePro\Platform\Manager\Customer $platformCustomerManager,
+        \Swarming\SubscribePro\Platform\Service\Customer $platformCustomerService,
         \Psr\Log\LoggerInterface $logger
     ) {
         $this->generalConfig = $generalConfig;
@@ -57,26 +55,41 @@ class SaveAfterData implements ObserverInterface
     {
         /** @var \Magento\Customer\Api\Data\CustomerInterface $customer */
         $customer = $observer->getData('customer_data_object');
-        // typecast is needed here because config implicitly treats a string as a website code, and int as an id
-        $websiteId = (int)$customer->getWebsiteId();
-        if (!$this->generalConfig->isEnabled($websiteId)
-        ) {
+
+        if (!$this->generalConfig->isEnabled((int)$customer->getWebsiteId())) {
             return;
         }
+
         /** @var \Magento\Customer\Api\Data\CustomerInterface $origCustomer */
         $origCustomer = $observer->getData('orig_customer_data_object');
-        // When a new customer is created, $origCustomer is null
-        // If there are no required changes, return.
         if ($origCustomer
-            && $origCustomer->getEmail() === $customer->getEmail()
-            && $origCustomer->getFirstname() === $customer->getFirstname()
-            && $origCustomer->getLastname() === $customer->getLastname()
-            && $origCustomer->getGroupId() === $customer->getGroupId()
+            && !$this->isCustomerChanged($customer, $origCustomer)
         ) {
             return;
         }
 
-        $this->savePlatformCustomer($customer);
+        try {
+            $this->savePlatformCustomer($customer);
+        } catch (\Exception $e) {
+            $this->logger->critical($e);
+        }
+    }
+
+    /**
+     * @param \Magento\Customer\Api\Data\CustomerInterface $customer
+     * @param \Magento\Customer\Api\Data\CustomerInterface $origCustomer
+     * @return bool
+     */
+    private function isCustomerChanged(CustomerInterface $customer, CustomerInterface $origCustomer): bool
+    {
+        if ($origCustomer->getEmail() === $customer->getEmail()
+            && $origCustomer->getFirstname() === $customer->getFirstname()
+            && $origCustomer->getLastname() === $customer->getLastname()
+            && $origCustomer->getGroupId() === $customer->getGroupId()
+        ) {
+            return false;
+        }
+        return true;
     }
 
     /**
@@ -86,28 +99,29 @@ class SaveAfterData implements ObserverInterface
     private function savePlatformCustomer($customer): void
     {
         $websiteId = $customer->getWebsiteId();
-        // Apple Pay will not work for new customers if they are not available on the S-Pro end,
-        // but it is not necessary to keep new customers for the other gateways
-        $createIfNotExist = $this->generalConfig->isApplePayEnabled($websiteId);
-        $customerId = (int)$customer->getId();
-        try {
-            $platformCustomer = $this->platformCustomerManager->getCustomerByMagentoCustomerId(
-                $customerId,
-                $createIfNotExist,
-                $websiteId
-            );
-            if (!$platformCustomer) {
-                return;
-            }
 
+        $platformCustomer = $this->platformCustomerManager->getCustomerByMagentoCustomerId(
+            (int)$customer->getId(),
+            $this->isCreateNewCustomerIfNotExist($websiteId),
+            $websiteId
+        );
+
+        if ($platformCustomer) {
             $platformCustomer->setFirstName($customer->getFirstname());
             $platformCustomer->setLastName($customer->getLastname());
             $platformCustomer->setEmail($customer->getEmail());
             $platformCustomer->setMagentoCustomerGroupId($customer->getGroupId());
 
-            $this->platformCustomerService->saveCustomer($platformCustomer, $websiteId);
-        } catch (\Exception $e) {
-            $this->logger->critical($e);
+            $this->platformCustomerService->saveCustomer($platformCustomer, (int)$websiteId);
         }
+    }
+
+    /**
+     * @param int|string|null $websiteId
+     * @return bool
+     */
+    private function isCreateNewCustomerIfNotExist($websiteId): bool
+    {
+        return $this->generalConfig->isApplePayEnabled($websiteId);
     }
 }
