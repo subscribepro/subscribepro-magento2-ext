e3d0e7aa277b16663722f0c1a32883cb65cdb9c9
Grayson Blanchard <grayson.blanchard@subcribepro.com>
2022-02-17 16:47:12 -0500

    Bump version to 1.5.11.

--- Block/Adminhtml/System/Config/InstalledVersion.php
+++ Block/Adminhtml/System/Config/InstalledVersion.php
@@ -4,7 +4,7 @@ namespace Swarming\SubscribePro\Block\Adminhtml\System\Config;
 
 class InstalledVersion extends \Magento\Config\Block\System\Config\Form\Field
 {
-    const INSTALLED_VERSION = '1.5.10';
+    const INSTALLED_VERSION = '1.5.11';
 
     protected function _getElementHtml(\Magento\Framework\Data\Form\Element\AbstractElement $element)
     {
