1eb911d1fe43c742b18df45fa487927dea4085f2
Grayson Blanchard <grayson.blanchard@subcribepro.com>
2022-03-08 14:53:16 -0500

    [#181501419] Apply patch from DK for EQP fixes.

---
+++ Setup/Patch/Data/AddSubscriptionEnabledProductAttributePatch.php
@@ -0,0 +1,95 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Swarming\SubscribePro\Setup\Patch\Data;
+
+use Magento\Catalog\Model\Product;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Framework\Setup\Patch\DataPatchInterface;
+use Magento\Framework\Setup\Patch\PatchVersionInterface;
+use Magento\Eav\Setup\EavSetupFactory;
+use Magento\Eav\Model\Entity\Attribute\ScopedAttributeInterface;
+use Magento\Eav\Model\Entity\Attribute\Source\Boolean;
+
+/**
+* Patch is mechanism, that allows to do atomic upgrade data changes
+*/
+class AddSubscriptionEnabledProductAttributePatch implements
+    DataPatchInterface
+{
+    /**
+     * @var ModuleDataSetupInterface $moduleDataSetup
+     */
+    private $moduleDataSetup;
+
+    /**
+     * @var \Magento\Eav\Setup\EavSetupFactory
+     */
+    private $eavSetupFactory;
+
+    /**
+     * @param ModuleDataSetupInterface $moduleDataSetup
+     * @param EavSetupFactory $eavSetupFactory
+     */
+    public function __construct(ModuleDataSetupInterface $moduleDataSetup, EavSetupFactory $eavSetupFactory)
+    {
+        $this->moduleDataSetup = $moduleDataSetup;
+        $this->eavSetupFactory = $eavSetupFactory;
+    }
+
+    /**
+     * Do Upgrade
+     *
+     * @return void
+     */
+    public function apply()
+    {
+        $eavSetup = $this->eavSetupFactory->create(['setup' => $this->moduleDataSetup]);
+        $eavSetup->addAttribute(
+            Product::ENTITY,
+            'subscription_enabled',
+            [
+                'type' => 'int',
+                'backend' => '',
+                'frontend' => '',
+                'group' => 'Subscribe Pro',
+                'label' => 'Subscription Enabled',
+                'input' => 'select',
+                'class' => '',
+                'source' => Boolean::class,
+                'global' => ScopedAttributeInterface::SCOPE_WEBSITE,
+                'visible' => true,
+                'required' => false,
+                'user_defined' => false,
+                'default' => '0',
+                'searchable' => false,
+                'filterable' => false,
+                'comparable' => false,
+                'visible_on_front' => false,
+                'used_in_product_listing' => false,
+                'unique' => false,
+                'apply_to' => 'simple,configurable,bundle,virtual,downloadable'
+            ]
+        );
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getAliases()
+    {
+        return [];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public static function getDependencies()
+    {
+        return [];
+    }
+}
