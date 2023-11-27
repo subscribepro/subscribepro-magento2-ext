fd9eb91fe8e8de23521289fe6128c997effdc538
EugeneShab <dev.eugene.shab@gmail.com>
2023-11-27 15:04:57 +0200

    feat: Add payment fields validation on checkout page.
    Refs: #197

--- view/base/requirejs-config.js
+++ view/base/requirejs-config.js
@@ -3,7 +3,7 @@ var config = {
     map: {
         '*': {
             spreedly: 'https://core.spreedly.com/iframe/iframe-v1.min.js',
-            paymentFields: 'https://hosted.subscribepro.com/payment-fields/payment-fields-0.9.4.js'
+            paymentFields: 'https://hosted.subscribepro.com/payment-fields/payment-fields-1.0.1.js'
         }
     }
 };
