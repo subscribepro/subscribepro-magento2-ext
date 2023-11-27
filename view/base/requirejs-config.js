2fa4ef8d0d2a206b57aea2ad6f94c9f8004bc488
EugeneShab <dev.eugene.shab@gmail.com>
2023-11-27 17:01:33 +0200

    feat: Update PaymentFields js version in require
    Refs: #197

--- view/base/requirejs-config.js
+++ view/base/requirejs-config.js
@@ -3,7 +3,7 @@ var config = {
     map: {
         '*': {
             spreedly: 'https://core.spreedly.com/iframe/iframe-v1.min.js',
-            paymentFields: 'https://hosted.subscribepro.com/payment-fields/payment-fields-1.0.1.js'
+            paymentFields: 'https://hosted.subscribepro.com/payment-fields/payment-fields-1.0.2.js'
         }
     }
 };
