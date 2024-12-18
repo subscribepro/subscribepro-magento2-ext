5318ba09ad25556cd5384d193327c055a5770cea
Garth Brantley <garth@subscribepro.com>
2024-12-18 16:17:14 -0500

    feat: assign additional payment data to quote payment
    Refs: #291

--- Observer/Payment/DataAssigner.php
+++ Observer/Payment/DataAssigner.php
@@ -4,6 +4,8 @@ namespace Swarming\SubscribePro\Observer\Payment;
 
 use Magento\Framework\Event\Observer;
 use Magento\Quote\Api\Data\PaymentInterface;
+use Magento\Sales\Api\Data\OrderPaymentInterface;
+use SubscribePro\Service\PaymentProfile\PaymentProfileInterface;
 use SubscribePro\Service\Transaction\TransactionInterface;
 use Swarming\SubscribePro\Gateway\Request\PaymentDataBuilder;
 
@@ -15,6 +17,9 @@ class DataAssigner extends \Magento\Payment\Observer\AbstractDataAssignObserver
     protected $additionalInformationList = [
         PaymentDataBuilder::PAYMENT_METHOD_TOKEN,
         TransactionInterface::BROWSER_INFO,
+        PaymentProfileInterface::CREDITCARD_FIRST_DIGITS,
+        PaymentProfileInterface::CREDITCARD_LAST_DIGITS,
+        PaymentProfileInterface::CREDITCARD_TYPE,
     ];
 
     /**
@@ -37,5 +42,8 @@ class DataAssigner extends \Magento\Payment\Observer\AbstractDataAssignObserver
                 $paymentInfo->setAdditionalInformation($key, $additionalData[$key]);
             }
         }
+        $paymentInfo->setData(OrderPaymentInterface::CC_EXP_MONTH, $additionalData[OrderPaymentInterface::CC_EXP_MONTH] ?? '');
+        $paymentInfo->setData(OrderPaymentInterface::CC_EXP_YEAR, $additionalData[OrderPaymentInterface::CC_EXP_YEAR] ?? '');
+        $paymentInfo->setData(OrderPaymentInterface::CC_TYPE, $additionalData['creditcard_type'] ?? '');
     }
 }
